94f1e3a6823c7aa4bb35b8cfa1d1d515b4478c71
Bhuwan pandeya <bpandeya70@gmail.com>
2018-07-03 22:09:09 +0545

    find even or odd number

---
+++ age_after_100_years.py
@@ -0,0 +1,8 @@
+import datetime
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+year = str((2014 - age)+100)
+print(name + " will be 100 years old in the year " + year)
+print("This is my frist python practice code")
+date = datetime.datetime.now()
+print(date)
